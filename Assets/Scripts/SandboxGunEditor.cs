1e39a894fa9e13a37bad77bcde54190d454da913
Austin O'Brien <elinguate.dev@gmail.com>
2021-04-19 09:58:26 +1200

    Tagged Gun gameObject with Player tag. Added small editor script to draw firing origin and firing direction.

---
+++ Assets/Scripts/SandboxGunEditor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SandboxGun))]
+public class SandboxGunEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        SandboxGun sandboxGun = GameObject.FindGameObjectWithTag("Player").GetComponent<SandboxGun>();
+        Handles.color = Color.white;
+        Handles.DrawLine(sandboxGun.m_FiringOrigin.position, (sandboxGun.m_FiringOrigin.position + sandboxGun.m_FiringOrigin.forward));
+    }
+}
