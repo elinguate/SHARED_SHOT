161ec3461bab5990b2f917cf4218782eb9373a4c
Nerys <NerysThamm@gmail.com>
2021-04-19 09:49:48 +1200

    Added basic ADS functionality

---
+++ Assets/Scripts/GunADS.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunADS : MonoBehaviour
+{
+    public SandboxGun gun;
+    public Camera maincamera;
+    public CharacterMotorData MotorData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            maincamera.fieldOfView = 50;
+            MotorData.m_MoveSpeed = 3;
+        }
+        else
+        {
+            maincamera.fieldOfView = 100;
+            MotorData.m_MoveSpeed = 12;
+        }
+    }
+}
